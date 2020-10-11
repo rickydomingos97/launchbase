86aff3584db399b1d158ffd60a658d0b29964af7
Ricky Domingos <ricardodomingos97@gmail.com>
2020-10-11 18:20:31 -0300

    desafio1.3.js done

--- desafio1.3/desafio1.3.js
+++ desafio1.3/desafio1.3.js
@@ -1,10 +1,41 @@
-const users = [
-    {name: 'Carlos', tecnologies: ['HTML', 'CSS']},
-    {name: 'Jasmine', tecnologies: ['Javascript', 'CSS']},
-    {name: 'Tuane', tecnologies:['HTML', 'Node.js']},
-    {name: 'Ricardo', tecnologies: ['HTML', 'CSS']}
+const users = [{
+        name: 'Carlos',
+        tecnologies: ['HTML', 'CSS']
+    },
+    {
+        name: 'Jasmine',
+        tecnologies: ['Javascript', 'CSS']
+    },
+    {
+        name: 'Tuane',
+        tecnologies: ['HTML', 'Node.js']
+    },
+    {
+        name: 'Ricardo',
+        tecnologies: ['HTML', 'CSS']
+    },
+    {
+        name: 'Ira',
+        tecnologies: ['HTML', 'Javascript', 'CSS']
+    }
 ]
 
 for (let user of users) {
-    console.log(`${user.name} trabalha com ${user.tecnologies.join(', ')}`)
+    console.log(`${user.name} work with ${user.tecnologies.join(', ')}`)
+}
+
+function checaSeUsuarioUsaCSS(user) {
+    for (let tecnology of user.tecnologies) {
+        if (tecnology == 'CSS')
+        return true
+    }
+    return false
+}
+
+for (let i = 0; i < users.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(users[i]);
+
+    if(usuarioTrabalhaComCSS) {
+        console.log(`The user ${users[i].name} works with CSS`)
+    }
 }
