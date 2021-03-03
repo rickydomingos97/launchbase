9ff66090a690c87ec5ab4c3e7f793210122778f7
rickydomingos97 <ricardodomingos97@gmail.com>
2021-03-03 16:00:53 -0300

    card add and css grid add

--- modulo4/public/scripts.js
+++ modulo4/public/scripts.js
@@ -1,13 +1,3 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
-/*
-para usar a função click em todos os cards
-*/
-for(let card of cards) {
-    card.addEventListener("click", function() {
-        const videoId = card.getAttribute("id")
-        window.location.href = `video?id=${videoId}`
-    })
-}
+
 
 
