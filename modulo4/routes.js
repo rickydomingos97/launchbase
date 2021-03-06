1500d6a89812ed78562c0c93949e6149ae18cdff
Ricky Domingos <ricardodomingos97@gmail.com>
2021-03-05 21:43:30 -0300

    avatar dinamico random unsplash

--- modulo4/routes.js
+++ modulo4/routes.js
@@ -10,6 +10,10 @@ routes.get('/instructors', (req, res) => {
     return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', (req, res) => {
+    return res.render("instructors/create")
+})
+
 routes.get('/members', (req, res) => {
     return res.send("members")
 })
